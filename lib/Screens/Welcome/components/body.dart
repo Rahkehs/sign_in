5b5ded9b8da2aea6591a38ef00136ba8aa3c5c13
Shekhar Shrestha <shekharshrestha27@gmail.com>
2022-04-06 22:42:58 +1000

    Final Commit

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Positioned(
+              top: 1,
+              right: 10,
+              child:
+              Image.asset (
+                "assets/icons/mmdsmall.png",
+                height: size.height * 0.45,
+              ),
+            ),
+
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text:  "Get Started",
+              color: Colors.white,
+              textColor: Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,52 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final Function press;
+  final Color color, textColor;
+  const RoundedButton({
+    Key key,
+    this.text,
+    this.press,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white30, style(
+        fonts
+        )
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 12),
+      width: size.width * 0.65,
+      height: size.height * 0.078,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(22),
+        child: newElevatedButton(),
+      ),
+    );
+  }
+
+  //Used:ElevatedButton as FlatButton is deprecated.
+  //Here we have to apply customizations to Button by inheriting the styleFrom
+
+  Widget newElevatedButton() {
+    return ElevatedButton(
+      child: Text(
+        text,
+        style: TextStyle(color: Colors.black,
+        fontWeight: FontWeight.bold),
+      ),
+      onPressed: press,
+      style: ElevatedButton.styleFrom(
+          primary: color,
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+          textStyle: TextStyle(
+              color: textColor, fontSize: 14, fontWeight: FontWeight.w500)),
+    );
+  }
+}
