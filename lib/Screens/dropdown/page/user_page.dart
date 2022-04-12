a7e01d2d391b3576371e4de93310d79e493883cf
Rahkehs <shekharshrestha27@gmail.com>
2022-04-12 21:36:33 +1000

    commit 12/04/2022 1.0

--- lib/Screens/dropdown/page/user_page.dart
+++ lib/Screens/dropdown/page/user_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_auth/Screens/Welcome/components/body.dart';
 class UserPage extends StatelessWidget {
   final String name;
   final String urlImage;
@@ -11,18 +11,24 @@ class UserPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
+  Widget build(BuildContext context) =>
+
+      Scaffold(
+
     appBar: AppBar(
-      backgroundColor: Color.fromARGB(255, 124, 115, 63),
+      backgroundColor: Color.fromARGB(255, 28, 28, 26),
       title: Text(name),
       centerTitle: true,
     ),
-    body:
-    Image.network(
-      urlImage,
-      width: 200,
-      height: 200,
-      fit: BoxFit.cover,
+    body: Row( mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        CircleAvatar(
+          radius: 110.00,
+          foregroundColor: Colors.black45,
+          backgroundImage: AssetImage ("assets/images/sidd.jpg"),
+        )
+      ],
     ),
   );
 }
